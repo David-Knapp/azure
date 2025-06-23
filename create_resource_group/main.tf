08b2893907f3efacfb274083c63833ed522776e0
David-Knapp <155780173+David-Knapp@users.noreply.github.com>
2025-06-22 20:04:44 -0700

    Update main.tf

--- create_resource_group/main.tf
+++ create_resource_group/main.tf
@@ -1,5 +1,5 @@
 # Create a resource group using the generated random name
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "create_resource_group" {
   location = var.resource_group_location
   name     = var.resource_group_name
 }
