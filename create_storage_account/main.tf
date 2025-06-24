b76437bb0b9e620c34456aa9e7d6b7e54c9a40f8
David-Knapp <155780173+David-Knapp@users.noreply.github.com>
2025-06-24 09:08:04 -0700

    Update and rename main.tf to variables.tf

--- create_storage_account/main.tf
+++
@@ -1,7 +0,0 @@
-# Variables
-
-variable "resoure_group_name" {
-  type = string
-}
-
-variable
---
+++ create_storage_account/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "westus2"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "resource_group_example"
+  description = "Name of the resource group."
+}
+
+variable "subscription_id" {
+  type = string
+}
