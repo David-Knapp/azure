34b73862d1c0ef2f014cec25e435a7cbf47f341c
David-Knapp <155780173+David-Knapp@users.noreply.github.com>
2025-06-24 09:26:11 -0700

    Update main.tf

--- create_storage_account/main.tf
+++ create_storage_account/main.tf
@@ -11,6 +11,6 @@ resource "azurerm_storage_account" "storage_account" {
   resource_group_name = var.resource_group_name
   location = var.resource_group_location
   account_tier = var.storage_account_tier
-  account_replication_type = var.storage_account_type
+  account_replication_type = var.storage_account_replication_type
 }
 
