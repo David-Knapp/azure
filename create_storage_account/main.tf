814b8667891e3087e98b93454b4e7ca3435f6cb7
David-Knapp <155780173+David-Knapp@users.noreply.github.com>
2025-06-24 18:44:22 -0700

    Update main.tf

--- create_storage_account/main.tf
+++ create_storage_account/main.tf
@@ -4,13 +4,19 @@ resource "azurerm_integer" "storage_account_number" {
   max = 999999
 }
 
-#
-
+# Create the storage account
 resource "azurerm_storage_account" "storage_account" {
-  name = 
+  name = "${lower(var.storage_account_prefix)}${random_integer.storage_account_number.result}"
   resource_group_name = var.resource_group_name
   location = var.resource_group_location
   account_tier = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
+}
+
+# Create the container for the storage account
+
+resource "azurerm_storage_container" "container" {
+  name = ""container"
+  storage_account_name = azurerm_storage_account.storage_account.name
 }
 
